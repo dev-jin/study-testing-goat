e329893d471fb47adb6cb9b516bb3b1bb2c65cf1
Kim Jin Hee <mail@kimjinhee.me>
2018-04-30 23:07:01 +0900

    기능 테스트가 작업 아이템을 추가할 수 있는지 확인

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,17 +17,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # 웹 페이지 타이틀과 헤더가 'To-Do'로 표시됨 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
         # 작업 추가
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                '작업 아이템 입력'
+        )
         
         # "책상정리" 라고 텍스트 상자에 입력하기
+        inputbox.send_keys('책상정리')
         
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에 "1: 책상정리" 아이템이 표시됨
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: 책상정리' for row in rows),
+        )
         
         # 추가 아이템 입력이 가능한 텍스트 상자가 있음
-        
         # "서랍정리"라고 텍스트 입력
+        self.fail('Finish the test!')
+
         
         # 페이지 갱신 후 두개의 아이템을 확인 가능
         
