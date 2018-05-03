dd2005718afed97fcba230168f1f04d0aaf6f524
Kim Jin Hee <mail@kimjinhee.me>
2018-05-04 03:23:00 +0900

    기능코드 리팩터링
    * Three strikes and Refacotr

--- functional_tests.py
+++ functional_tests.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
-    
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -11,11 +12,16 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 작업 목록 온라인 앱 사이트 접속
         self.browser.get('http://localhost:8000')
 
-        # 웹 페이지 타이틀과 헤더가 'To-Do'로 표시됨 
+        # 웹 페이지 타이틀과 헤더가 'To-Do'로 표시됨
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -36,7 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
 
-        self.assertIn('1: 책상정리', [row.text for row in rows])
+        self.check_for_row_in_list('1: 책상정리')
 
         # 추가 아이템 입력이 가능한 텍스트 상자가 있음
         # "서랍정리"라고 텍스트 입력
@@ -45,17 +51,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 페이지 갱신 후 두개의 아이템을 확인 가능
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 책상정리', [row.text for row in rows])
-        self.assertIn('2: 서랍정리', [row.text for row in rows])
+        self.check_for_row_in_list('2: 서랍정리')
+        self.check_for_row_in_list('1: 책상정리')
 
         # 사이트는 개인별로 특정 URL을 생성해 준다
 
         # URL에 대한 설명도 같이 표시
 
         # 해당 URL에 접속하면 개인의 작업목록을 확인 가능
-       
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
